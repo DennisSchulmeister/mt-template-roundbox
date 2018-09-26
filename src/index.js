f51103fbe131b55afe6c27a8bb47dcdc6f602db7
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-27 01:00:56 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import MiniTutorial from "mini-tutorial.js";
+import mtTheme from "mini-tutorial.js/themes/roundbox.css";
+
+import myStyle from "./style.css";
+
+window.addEventListener("load", () => {
+    let mt = new MiniTutorial();
+    mt.start();
+});
