63167e473e95857e1e008123219137cb1ecc5fb6
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-27 12:32:56 +0200

    Allow splitting a large document into several HTML files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import MiniTutorial from "mini-tutorial.js";
 import mtTheme from "mini-tutorial.js/themes/roundbox.css";
+import mtThemeCommon from "mini-tutorial.js/themes/common.css";
+import myStylesheet from "./style.css";
 
-import myStyle from "./style.css";
+async function fetchChapters(chapters, callback) {
+    let response = null;
+    let html = "";
 
-window.addEventListener("load", () => {
+    for(let i = 0; i < chapters.length; i++) {
+        response = await fetch(chapters[i]);
+        html += await response.text();
+    }
+
+    if (document.readyState == "complete") {
+        callback(html);
+    } else {
+        window.addEventListener("load", () => callback(html));
+    }
+}
+
+function startApplication(html) {
+    if (html) {
+        let main = document.querySelector("main");
+        main.innerHTML = html;
+    }
+
     let mt = new MiniTutorial();
     mt.start();
-});
+}
+
+fetchChapters([
+    // Add HTML files here if you want to split the document.
+    // The HTML files should only contain <section>-elements without <html>/<head>/<body>
+    //
+    // "01-intro.html",
+    // "02-chapter2.html",
+    // ...
+], startApplication);
